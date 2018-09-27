c20b580f80dd32d908dd01974d5105debeeec685
lantran <tttlan1503code@gmail.com>
2018-09-27 14:19:50 +0700

    demo react-js

--- src/components/shared/AppBreadcrumb.component.js
+++ src/components/shared/AppBreadcrumb.component.js
@@ -5,13 +5,34 @@ import Breadcrumb from 'react-bootstrap/lib/Breadcrumb';
 import './app-breadcrumb.scss';
 
 class AppBreadcrumb extends Component {
+    checkActiveBreadcrumbItem(pathName) {
+        const currentPathName = window.location.pathname;
+        let active = false;
+
+        switch (currentPathName) {
+            case pathName:
+                active = true;
+                break;
+            case pathName:
+                active = true;
+                break;
+            default:
+                break;
+
+        }
+
+        console.log(active);
+        return active;
+    }
+
     render() {
         return (
             <Breadcrumb className="app-breadcrumb">
-                <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
-                <Breadcrumb.Item href="/cart">
-                    Shopping Cart
-                </Breadcrumb.Item>
+                <Breadcrumb.Item active={this.checkActiveBreadcrumbItem('/')}>Home</Breadcrumb.Item>
+                {
+                    this.checkActiveBreadcrumbItem('/cart') ?
+                        <Breadcrumb.Item active={this.checkActiveBreadcrumbItem('/cart')}>Shopping Cart</Breadcrumb.Item> : ''
+                }
             </Breadcrumb>
         );
     }
